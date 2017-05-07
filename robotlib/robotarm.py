b2ed6d2dab6d5ff418c08119e8a53e9dff62740c
bigmstone <matthew99@gmail.com>
2017-05-07 15:33:32 -0400

    Adding toruqe state. Adding proper render of return

--- robotlib/robotarm.py
+++ robotlib/robotarm.py
@@ -10,14 +10,6 @@ class PhantomX(object):
     """ PhantomX class for controlling customized PhantomX Reactor
     """
     def __init__(self):
-        self._arm_state = {
-            "grip": 0,
-            "hand": 0,
-            "wrist": 0,
-            "elbow": 0,
-            "shoulder": 0,
-        }
-
         self.ax12 = Ax12()
 
     def grip(self, location):
@@ -26,12 +18,22 @@ class PhantomX(object):
         if location < 0 or location > 1:
             raise ValueError("Incorrect value for location")
         position = int(location * 500)
-        self.ax12.move(constants.MOTOR_ID_GRIP, position)
+        self.ax12.moveSpeed(constants.MOTOR_ID_GRIP, position, 200)
 
     def get_grip(self):
         """ Return location of servo
         """
-        return self.ax12.readPosition(constants.MOTOR_ID_GRIP)
+        position = self.ax12.readPosition(constants.MOTOR_ID_GRIP) / 500
+
+        return position
+
+    def set_grip_state(self, state):
+        """ Set torque state of servo
+        """
+        if not isinstance(state, bool):
+            raise ValueError("State must be of type bool")
+
+        return self.ax12.setTorqueStatus(constants.MOTOR_ID_GRIP, state)
 
     def wrist(self, location):
         """ Move wrist to desired location
@@ -39,12 +41,25 @@ class PhantomX(object):
         if location < 0 or location > 1:
             raise ValueError("Incorrect value for location")
         position = int(location * 600 + 200)
-        self.ax12.move(constants.MOTOR_ID_WRIST, position)
+        self.ax12.moveSpeed(constants.MOTOR_ID_WRIST, position, 200)
 
     def get_wrist(self):
         """ Return location of servo
         """
-        return self.ax12.readPosition(constants.MOTOR_ID_WRIST)
+        position = (
+            (self.ax12.readPosition(constants.MOTOR_ID_WRIST) - 200) /
+            600
+        )
+
+        return position
+
+    def set_wrist_state(self, state):
+        """ Set torque state of servo
+        """
+        if not isinstance(state, bool):
+            raise ValueError("State must be of type bool")
+
+        return self.ax12.setTorqueStatus(constants.MOTOR_ID_WRIST, state)
 
     def hand(self, location):
         """ Move hand to desired location
@@ -52,12 +67,25 @@ class PhantomX(object):
         if location < 0 or location > 1:
             raise ValueError("Incorrect value for location")
         position = int(location * 600 + 200)
-        self.ax12.move(constants.MOTOR_ID_HAND, position)
+        self.ax12.moveSpeed(constants.MOTOR_ID_HAND, position, 200)
 
     def get_hand(self):
         """ Return location of servo
         """
-        return self.ax12.readPosition(constants.MOTOR_ID_HAND)
+        position = (
+            (self.ax12.readPosition(constants.MOTOR_ID_HAND) - 200) /
+            600
+        )
+
+        return position
+
+    def set_hand_state(self, state):
+        """ Set torque state of servo
+        """
+        if not isinstance(state, bool):
+            raise ValueError("State must be of type bool")
+
+        return self.ax12.setTorqueStatus(constants.MOTOR_ID_HAND, state)
 
     def elbow(self, location):
         """ Move elbow to desired location
@@ -65,14 +93,30 @@ class PhantomX(object):
         if location < 0 or location > 1:
             raise ValueError("Incorrect value for location")
         position = int(location * 600 + 200)
-        self.ax12.move(constants.MOTOR_ID_ELBOW[0], position)
+        self.ax12.moveSpeed(constants.MOTOR_ID_ELBOW[0], position, 200)
         position = int((1 - location) * 600 + 200)
-        self.ax12.move(constants.MOTOR_ID_ELBOW[1], position)
+        self.ax12.moveSpeed(constants.MOTOR_ID_ELBOW[1], position, 200)
 
     def get_elbow(self):
         """ Return location of servo
         """
-        return self.ax12.readPosition(constants.MOTOR_ID_ELBOW[0])
+        position = (
+            (self.ax12.readPosition(constants.MOTOR_ID_ELBOW[0]) - 200) /
+            600
+        )
+
+        return position
+
+    def set_elbow_state(self, state):
+        """ Set torque state of servo
+        """
+        if not isinstance(state, bool):
+            raise ValueError("State must be of type bool")
+
+        self.ax12.setTorqueStatus(constants.MOTOR_ID_ELBOW, state)
+        self.ax12.setTorqueStatus(constants.MOTOR_ID_ELBOW, state)
+
+        return True
 
     def shoulder(self, location):
         """ Move shoulder to desired location
@@ -80,14 +124,30 @@ class PhantomX(object):
         if location < 0 or location > 1:
             raise ValueError("Incorrect value for location")
         position = int(location * 600 + 200)
-        self.ax12.move(constants.MOTOR_ID_SHOULDER[0], position)
+        self.ax12.moveSpeed(constants.MOTOR_ID_SHOULDER[0], position, 200)
         position = int((1 - location) * 600 + 200)
-        self.ax12.move(constants.MOTOR_ID_SHOULDER[1], position)
+        self.ax12.moveSpeed(constants.MOTOR_ID_SHOULDER[1], position, 200)
 
     def get_shoulder(self):
         """ Return location of servo
         """
-        return self.ax12.readPosition(constants.MOTOR_ID_SHOULDER[0])
+        position = (
+            (self.ax12.readPosition(constants.MOTOR_ID_SHOULDER[0]) - 200) /
+            600
+        )
+
+        return position
+
+    def set_shoulder_state(self, state):
+        """ Set torque state of servo
+        """
+        if not isinstance(state, bool):
+            raise ValueError("State must be of type bool")
+
+        self.ax12.setTorqueStatus(constants.MOTOR_ID_SHOULDER[0], state)
+        self.ax12.setTorqueStatus(constants.MOTOR_ID_SHOULDER[1], state)
+
+        return True
 
     def pivot(self, location):
         """ Move pivot to desired location
@@ -95,10 +155,22 @@ class PhantomX(object):
         if location < 0 or location > 1:
             raise ValueError("Incorrect value for location")
         position = int(location * 600 + 200)
-        self.ax12.move(constants.MOTOR_ID_PIVOT, position)
+        self.ax12.moveSpeed(constants.MOTOR_ID_PIVOT, position, 200)
 
     def get_pivot(self):
         """ Return location of servo
         """
-        return self.ax12.readPosition(constants.MOTOR_ID_PIVOT)
+        position = (
+            (self.ax12.readPosition(constants.MOTOR_ID_PIVOT) - 200) /
+            600
+        )
 
+        return position
+
+    def set_pivot_state(self, state):
+        """ Set torque state of servo
+        """
+        if not isinstance(state, bool):
+            raise ValueError("State must be of type bool")
+
+        return self.ax12.setTorqueStatus(constants.MOTOR_ID_PIVOT, state)
